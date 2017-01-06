53b7acd80e82d36844be92f5556813223c5f714e
Thanh <dang.phongvan.thanh@gmail.com>
2017-01-06 16:58:17 +0700

    add remote stream

--- app.js
+++ app.js
@@ -1,1 +1,79 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser = require("body-parser"),
+    morgan = require("morgan"),
+    N = require("./nuve"),
+    config = require('./licode_config');
+
+var app = express();
+var port = process.env.port || 3000;
+app.use(morgan("dev"));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.set("view engine","ejs");
+
+N.API.init(config.nuve.superserviceID, config.nuve.superserviceKey, 'http://192.168.1.21:3000/');
+
+app.get("/",function(req,res){
+    getRoomIDByRoomName("basicExampleRoom",function(roomID){
+        if(roomID == null || roomID == undefined ){
+               roomID = "Room doesn't have exists. Please check again !!"
+        } 
+        res.render("index",{roomID: roomID});
+    });
+    
+});
+
+
+var getRoomIDByRoomName = function(roomName,callback){
+    var roomID;
+    N.API.getRooms(function(roomList){
+        var rooms = JSON.parse(roomList);
+        for (var i in rooms){
+            if(rooms[i].name == roomName){
+                roomID = rooms[i]._id;
+                break;
+            }
+        }
+        callback(roomID);
+    });
+};
+
+var createToken = function (roomID, username,role,callback){
+    console.log("Access createToken");
+    console.log("RoomI: "+ roomID + " - User: "+username+" - Role: "+role);
+    N.API.createToken(roomID, username, role, function(token) {
+        console.log("token: ",token);
+        callback(token);
+    },function(err){
+        console.log("Token Error: ",err);
+    })
+}
+
+
+app.post("/joinRoom",function(req,res){
+    console.log("POST JOIN ROOM");
+    var roomID = req.body.roomID;
+    var username = req.body.username;
+    var role = "presenter";
+
+    createToken(roomID,username,role,function(token){
+        if(token == null || token == undefined ){
+            res.status(500).send(JSON.stringify({error:"The Erizo have problem."}));
+        }
+
+        res.send(JSON.stringify({Token:token}));
+    });
+});
+
+app.listen(port,function(){
+    console.log("Server listening ....");
+    //console.log(config);
+})
+
---
+++ licode_config.js
@@ -0,0 +1,5 @@
+var config = {};
+config.nuve = {};
+config.nuve.superserviceID = '586c7c63de5cf0c6c21557d4';
+config.nuve.superserviceKey = '8123';
+module.exports = config;
