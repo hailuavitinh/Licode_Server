c7205c7852ddcfc2bbaff0cee2a6ae89a86a0283
Dang Chi Thanh <dang.phongvan.thanh@gmail.com>
2017-01-17 17:22:52 +0700

    template

--- public/angularjs/app.js
+++ public/angularjs/app.js
@@ -1,19 +1,34 @@
 var FConfApp = angular.module('FConfApp',['ngRoute']);
 
-FConfApp.config(function($routeProvider){
+FConfApp.config(function($routeProvider,$locationProvider){
     $routeProvider
         .when('/',{
-            templateUrl:'views/home.html',
+            templateUrl:'angularjs/views/home.html',
             controller:'mainController'
         })
         .when('/about',{
-            templateUrl:'views/home.html',
+            templateUrl:'angularjs/views/about.html',
             controller:'aboutController'
         })
-    
+        .when('/rooms',{
+            templateUrl:'angularjs/views/rooms.html',
+            controller:'roomsController'
+        });
+
+        $locationProvider.html5Mode(true);
 })
 
 FConfApp.controller('mainController',function($scope){
     console.log("Angular");
     $scope.message = 'main Controller';
+});
+
+FConfApp.controller('aboutController',function($scope){
+    console.log("about");
+    $scope.message='about Controller';
+});
+
+FConfApp.controller('roomsController',function($scope){
+    console.log("rooms");
+    $scope.message='rooms Controller';
 });
